be8f785828436703b54fbbced4a3672d0afe7351
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-10-01 01:56:55 +0300

    eslint setup

---
+++ obsidian/vaults/nodeJS/Express/ESLINT.md
@@ -0,0 +1,8 @@
+[[Express Framework]]
+
+```
+npm i eslint prettier eslint-config-prettier eslint-plugin-pre
+ttier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y esl
+int-plugin-react --save-dev
+```
+
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -8,7 +9,7 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log('================================');
   console.log(
-    `Running on PORT: 127.0.0.1:${port}\nNODE_ENV = ${process.env.NODE_ENV}`
+    `Running on PORT: 127.0.0.1:${port}\nNODE_ENV = ${process.env.NODE_ENV}`,
   );
   console.log('================================');
 });
