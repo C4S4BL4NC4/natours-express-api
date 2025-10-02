77bc9dead03fa62eae1e6effb566fe9568966594
C4S4BL4NC4 <sabahsadeq199@gmail.com>
2025-10-02 15:09:57 +0300

    fix mongoose

--- src/server.js
+++ src/server.js
@@ -1,15 +1,30 @@
 const dotenv = require('dotenv');
 
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  });
+
 const app = require('./app');
 
 // console.log(app.get('env'));
 // console.log(process.env);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log('================================');
   console.log(
     `Running on PORT: 127.0.0.1:${port}\nNODE_ENV = ${process.env.NODE_ENV}`,
   );
-  console.log('================================');
 });
