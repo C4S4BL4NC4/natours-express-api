f384ce2ad678bc7c3eab6ec70745fe9ad066d576
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-10-03 03:51:36 +0300

    mongoose fixed

--- src/server.js
+++ src/server.js
@@ -7,11 +7,10 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then((con) => {
     console.log(con.connections);
