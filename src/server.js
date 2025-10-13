70a629fcae4579376345def11af30035f3f77fb5
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-10-13 15:22:11 +0300

    server killing with unhandler and event listeners

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught exception!');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env', quiet: true });
 
 const DB = process.env.DATABASE.replace(
@@ -12,12 +18,19 @@ mongoose.connect(DB).then(() => console.log('=> Connected to Database.'));
 
 const app = require('./app');
 
-// console.log(app.get('env'));
-// console.log(process.env);
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(
     `Running on port: 127.0.0.1:${port}\nNODE_ENV = ${process.env.NODE_ENV}`,
   );
   console.log('=> Connecting...');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandler rejection! Shutting Down...');
+  server.close(() => {
+    console.log('=> Server closed.');
+    process.exit(1);
+  });
 });
