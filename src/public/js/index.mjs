0480b5fbd2ea32282424171d02c6a29ca5e9d923
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-11-30 17:49:41 +0300

    idnex mapbox is null fixme

---
+++ src/public/js/index.mjs
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import { login } from './login.mjs';
+import { displayMap } from './mapbox.mjs';
+
+const mapBox = document.querySelector('.map');
+console.log(mapBox);
+const loginForm = document.querySelector('.form');
+console.log(loginForm);
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
