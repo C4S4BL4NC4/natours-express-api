76dcd6bb0149df95d788e83e27ba625dd26bc507
C4S4BL4NC4 <74897764+C4S4BL4NC4@users.noreply.github.com>
2025-12-02 17:33:03 +0300

    /me fully implemented

--- src/public/js/updateSettings.mjs
+++ src/public/js/updateSettings.mjs
@@ -4,19 +4,18 @@ import axios from 'https://cdnjs.cloudflare.com/ajax/libs/axios/1.10.0/esm/axios
 
 import { showAlert } from './alerts.mjs';
 
-export const updateData = async (name, email) => {
+// type is either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    // http://127.0.0.1:3000/api/v1/users/updatePassword updateMe
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url: `http://127.0.0.1:3000/api/v1/users/${type === 'password' ? 'updatePassword' : 'updateMe'}`,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
